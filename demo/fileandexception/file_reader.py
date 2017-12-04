fdb4cd344d2315a1736d13efcc5d428c3ee6b7ac
kevin.zhang <453475634@qq.com>
2017-12-04 18:34:31 +0800

    增加逐行读取文件

--- demo/fileandexception/file_reader.py
+++ demo/fileandexception/file_reader.py
@@ -2,4 +2,28 @@
 """open在当前执行文件目录中查找对应的文件"""
 with open('pi_digits.txt')  as file_object:
     contents = file_object.read()
-    print(contents)
+    print(contents.rstrip())
+
+"""读取文件夹里面的文件"""
+with open("text_files\\test.txt")  as file_object1:
+    contents1 = file_object1.read()
+    print(contents1.rstrip())
+
+"""逐行读取文件"""
+
+print("#################################")
+fliename = "pi_digits.txt"
+with open(fliename) as file_object:
+    for line in file_object:
+        print(line.rstrip())
+
+
+"""将文件存储在一个列表中，外部进行调用"""
+
+print("#################################")
+filename = "pi_digits.txt"
+with open(filename) as file_object:
+    lines = file_object.readlines()
+
+for line in lines:
+    print(line.rstrip())
