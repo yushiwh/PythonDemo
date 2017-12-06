868badf10a99aac8cd0754016a201aec7c6e8b3c
kevin.zhang <453475634@qq.com>
2017-12-06 18:27:54 +0800

    绘制一搜飞船

--- demo/alien_invasion/alien_invasion.py
+++ demo/alien_invasion/alien_invasion.py
@@ -1,12 +1,21 @@
 import sys
 import pygame
+from settings import Settings
+from ship import Ship
 
 
 def run_game():
     """初始化游戏并且创建一个屏幕对象"""
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    """创建一搜飞船"""
+    ship = Ship(screen)
+
+    """设置背景颜色"""
+    bg_color = (230, 230, 230)
 
     """开始游戏的主循环"""
     while True:
@@ -14,6 +23,11 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        """每次循环的时候会重新绘制屏幕"""
+        screen.fill(ai_settings.bg_color)
+        """创建飞船"""
+        ship.blitme()
 
         """让最近绘制的屏幕可见"""
         pygame.display.flip()
